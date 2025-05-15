f210d47caccb76f77ad28a9464e62be433944907
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-05-15 16:31:10 +0545

    complete search function

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,9 @@
+from venv import logger
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from carts.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request):
@@ -14,7 +16,7 @@ def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)#get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))# get the cart using the cart_id present in the session
-    except Cart.DoesNotExist:
+    except ObjectDoesNotExist:
         cart = Cart.objects.create(
             cart_id=_cart_id(request)
         )
@@ -24,7 +26,7 @@ def add_cart(request, product_id):
         cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1 # if the product is already in the cart, increase the quantity
         cart_item.save()
-    except CartItem.DoesNotExist:
+    except ObjectDoesNotExist:
         cart_item = CartItem.objects.create(
             product=product,
             quantity=1,
@@ -53,7 +55,7 @@ def remove_cart_item(request, product_id):
     cart_item.delete()
     return redirect('cart')
 
-def cart(request, total=0, quantity=0, cart_items=None):
+def cart(request, total=0, quantity=0, tax=0, grand_total=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
@@ -62,8 +64,9 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except Cart.DoesNotExist:
-         pass
+    except ObjectDoesNotExist:
+          logger.error("Cart not found")
+      
      
     context = {
          'total': total,
